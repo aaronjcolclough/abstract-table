a5c500f95d33c003f6ec3b8d99f94a1a0b29d369
Aaron Colclough <42378208+aaronjcolclough@users.noreply.github.com>
2022-08-04 15:29:05 -0400

    initial

---
+++ abstract-base-table.component.ts
@@ -0,0 +1,43 @@
+export abstract class AbstractBaseTableComponent<T> {
+  dataSource: MatTableDataSource<T>;
+  selection: SelectionModel<T> = new SelectionModel<T>(false, []);
+  src: QuerySource<T>;
+  query: QueryResult<T>;
+
+  constructor(
+    protected dialog: MatDialog,
+    protected entType: string,
+    protected setStream: (entity:T) => void
+  ) { }
+
+  openDialog = (dialog: ComponentType<unkown>, options: DialogOptions) =>
+  this.dialog.open(dialog, { ...options }).afterClosed();
+
+  clear = () => {
+    this.selection.clear();
+    this.setStream(null);
+  };
+
+  selectRow = (e: T) => {
+    this.selection.toggle(e);
+
+    this.selection.selected.includes(e)
+      ? this.setStream()
+      : this.clear();
+  };
+
+  removeRow = () => this.dialog.open(ConfirmDialog, {
+    data: {
+      title: `Remove ${this.entityType}?`,
+      content: `Are you sure you want to remove this ${this.entityType}?`
+    },
+    disableClose: true,
+    autoFocus: false
+  }).afterClosed();
+
+  applyFilter = (filter: string) => {
+    this.src?.onSearch(filter);
+
+    this.dataSource?.paginator && this.dataSource.paginator.firstPage();
+  }
+}
